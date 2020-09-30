022845eb79058feb8662e8d67ab1d368cfe51982
JoshuaGDennis <joshuagarydennis@gmail.com>
2020-09-30 20:27:28 +0100

    adding to pokedex logic

--- src/pages/PokedexPage/PokedexPage.tsx
+++ src/pages/PokedexPage/PokedexPage.tsx
@@ -1,52 +1,67 @@
-import { getGeneration } from "helpers/api";
-import { getIdFromUrl } from "helpers/strings";
+import { useQuery } from "react-query";
 import { KeyValue } from "helpers/types";
+import { getGeneration } from "helpers/api";
+import { useParams } from "react-router-dom";
+import { getIdFromUrl } from "helpers/strings";
 import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
-
-interface iState {
-  pokemonList: KeyValue[];
-  offset: number;
-}
+import Button from "components/Button";
 
 // Number limit of how many pokemon to display per 'page'
 const LIST_LIMIT = 30;
 
 const PokedexPage: React.FC = () => {
-  const [state, setState] = useState<iState>({
-    pokemonList: [],
-    offset: 0,
-  });
-
   const { id } = useParams<{ id: string }>();
   const { data } = useQuery(id, getGeneration);
 
+  const [offset, setOffset] = useState<number>(0);
+  const [sortedData, setSortedData] = useState<KeyValue[]>([]);
+  const [pokemonList, setPokemonList] = useState<KeyValue[]>([]);
+
+  const updatePokemonList = () => {
+    if (sortedData.length) {
+      setPokemonList(sortedData.slice(offset, offset + LIST_LIMIT));
+    }
+  };
+
   useEffect(() => {
     if (data) {
-      setState((s) => ({
-        ...s,
-        pokemonList: data.pokemon_species.sort((a, b) => {
-          return +getIdFromUrl(a.url) - +getIdFromUrl(b.url);
-        }),
-      }));
+      setSortedData(
+        data.pokemon_species.sort(
+          (a, b) => +getIdFromUrl(a.url) - +getIdFromUrl(b.url)
+        )
+      );
     }
   }, [data]);
 
+  useEffect(updatePokemonList, [sortedData, offset]);
+
   useEffect(() => {
-    if (state.pokemonList) {
-      console.log(
-        state.pokemonList.slice(state.offset, LIST_LIMIT + state.offset)
-      );
+    if (pokemonList.length) {
+      console.log(pokemonList);
     }
-  }, [state.pokemonList, state.offset]);
+  }, [pokemonList]);
 
   return (
     <Container fluid>
       <Row>
         <Col>
           <h1>Pokedex Page!</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button onClick={() => setOffset((s) => s + LIST_LIMIT)}>
+            Load more
+          </Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          {pokemonList.map(({ name }) => (
+            <p>{name}</p>
+          ))}
         </Col>
       </Row>
     </Container>
