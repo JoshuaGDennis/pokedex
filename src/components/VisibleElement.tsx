40f6d9f3619b0a6a3c6455ca6e5fabee1d52d1e8
JoshuaGDennis <joshuagarydennis@gmail.com>
2020-12-02 16:48:11 +0000

    styling visible element

--- src/components/VisibleElement.tsx
+++ src/components/VisibleElement.tsx
@@ -1,23 +1,29 @@
-import React, { useRef } from 'react'
-import useVisibility from 'helpers/useVisibility'
+import React, { useRef } from "react";
+import useVisibility from "helpers/useVisibility";
+import styles from "styles/VisibleElement.module.scss";
 
 interface iProps {
-    onVisible?(): any
-    className?: string
-    once?: boolean
-    children?: React.ReactNode
+  onVisible?(): any;
+  className?: string;
+  once?: boolean;
+  children?: React.ReactNode;
 }
 
-const VisibilityElement: React.FC<iProps> = ({ onVisible = () => {}, className, once, children }) => {
-    const ref = useRef<HTMLDivElement>(null)
+const VisibilityElement: React.FC<iProps> = ({
+  onVisible = () => {},
+  className,
+  once,
+  children,
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
 
-    useVisibility(ref, {fn: onVisible, once})
+  useVisibility(ref, { fn: onVisible, once });
 
-    return (
-        <div ref={ref} className={className}>
-            {children}
-        </div>
-    )
-}
+  return (
+    <div ref={ref} className={`${styles.loader} ${className}`}>
+      {children}
+    </div>
+  );
+};
 
-export default VisibilityElement
+export default VisibilityElement;
