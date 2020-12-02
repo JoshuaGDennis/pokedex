d7675a04d6fa135ace55e926fd171f77780197eb
JoshuaGDennis <joshuagarydennis@gmail.com>
2020-12-02 17:15:48 +0000

    trying to simplify scss modules

--- src/components/PokedexCard.tsx
+++ src/components/PokedexCard.tsx
@@ -4,90 +4,98 @@ import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import LoadingCard from "./LoadingCard";
-import styles from "styles/PokedexCard.module.scss";
+import styles from "styles/cards.module.scss";
+import miscStyles from "styles/misc.module.scss";
 import React, { useEffect, useState, useRef } from "react";
-import { PokemonResponse, useTheme, useVisibility, capitalise, useApi } from 'helpers'
+import {
+  PokemonResponse,
+  useTheme,
+  useVisibility,
+  capitalise,
+  useApi,
+} from "helpers";
 
 interface iProps {
   id: string;
-  startLoad: boolean
-  loaded(): void
+  startLoad: boolean;
+  loaded(): void;
 }
 
-const PokedexCard:React.FC<iProps> = ({ id, startLoad, loaded }) => {
-  const ref = useRef<HTMLDivElement>(null)
-  
-  const [ isLoading, setIsLoading ] = useState(true)
-  const [ pokemon, setPokemon ] = useState<PokemonResponse | null>(null)
+const PokedexCard: React.FC<iProps> = ({ id, startLoad, loaded }) => {
+  const ref = useRef<HTMLDivElement>(null);
 
-  const theme = useTheme()
-  const { getPokemon } = useApi()
-  const inView = useVisibility(ref, { once: true })
+  const [isLoading, setIsLoading] = useState(true);
+  const [pokemon, setPokemon] = useState<PokemonResponse | null>(null);
+
+  const theme = useTheme();
+  const { getPokemon } = useApi();
+  const inView = useVisibility(ref, { once: true });
 
   useEffect(() => {
     if (inView && startLoad) {
-      getPokemon(id).then(data => {
-        setPokemon(data)
-        setIsLoading(false)
-      })
+      getPokemon(id).then((data) => {
+        setPokemon(data);
+        setIsLoading(false);
+      });
     }
-  }, [inView, startLoad, getPokemon, id])
+  }, [inView, startLoad, getPokemon, id]);
 
-  if (isLoading || !pokemon) return <LoadingCard cardRef={ref}/>
+  if (isLoading || !pokemon) return <LoadingCard cardRef={ref} />;
 
   return (
     <Link
-      className={styles.link}
+      className={miscStyles.link}
       to={{
         pathname: `/pokemon/${pokemon.name}`,
         state: pokemon,
       }}
     >
-      <Card
-        ref={ref}
-        className={[
-          styles.card, 
-          styles[`${pokemon.types[0]}${theme === 'dark' ? '--dark' : ''}`]
-        ].join(" ")}
-      >
-        <div className={styles.pokeball}>
-          <div className={styles.inner} />
+      <Card ref={ref} className={styles.cardSelectable}>
+        <div className={miscStyles[`pokeball_${pokemon.types[0]}`]}>
+          <div className={miscStyles.inner} />
         </div>
 
-        <Card.Body className={styles.body}>
+        <Card.Body
+          className={[
+            styles.cardBodyRounded,
+            `bg-${pokemon.types[0]}${theme === "dark" ? "--darker" : ""}`,
+          ].join(" ")}
+        >
           <Row>
             <Col>
-              <p className={styles.id}>#{pokemon.id}</p>
+              <p className={styles.cardId}>#{pokemon.id}</p>
             </Col>
           </Row>
           <Row>
             <Col>
-              <Image 
-                className={styles.image} 
-                src={pokemon.image} 
-                onLoad={loaded} 
-                fluid 
+              <Image
+                className={styles.cardImage}
+                src={pokemon.image}
+                onLoad={loaded}
+                fluid
               />
             </Col>
           </Row>
         </Card.Body>
-        <Card.Footer className={styles.footer}>
+        <Card.Footer className={styles.cardFooterRounded}>
           <Row>
             <Col>
-              <p className={styles.name}>{capitalise(pokemon.name)}</p>
+              <p className={styles.cardName}>{capitalise(pokemon.name)}</p>
             </Col>
           </Row>
-          <Row className={styles.types}>
+          <Row className="mt-3">
             {pokemon.types.map((type) => (
               <Col key={type}>
-                <p className={`bg-${type}`}>{capitalise(type)}</p>
+                <p className={`${styles.cardType} bg-${type}`}>
+                  {capitalise(type)}
+                </p>
               </Col>
             ))}
           </Row>
         </Card.Footer>
       </Card>
     </Link>
-  )
-}
+  );
+};
 
-export default PokedexCard
+export default PokedexCard;
