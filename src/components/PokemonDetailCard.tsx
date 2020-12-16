bd1b7c20a455a5e92bbe01deb19c8964825f528f
josh.dennis@awaze.co.uk <josh.dennis@awaze.co.uk>
2020-12-16 14:07:05 +0000

    style changes to tabs

--- src/components/PokemonDetailCard.tsx
+++ src/components/PokemonDetailCard.tsx
@@ -3,9 +3,10 @@ import {
   PokemonTypeResponse,
   PokemonResponse,
   SpeciesResponse,
+  useTheme,
 } from "helpers";
-import React from "react";
 import Tab from "react-bootstrap/Tab";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Tabs from "react-bootstrap/Tabs";
 import styles from "styles/PokemonCard.module.scss";
@@ -24,31 +25,40 @@ const PokemonDetailCard: React.FC<iProps> = ({
   types,
   abilities,
   species,
-}) =>  (
-  <Card className={styles.cardSplit}>
-    <Card.Body>
-      <Tabs className={`${styles.cardTabs} ${styles[`cardTabsColor${types[0].name}`]} justify-content-center mb-4`}>
-        <Tab eventKey="profile" title="PROFILE">
-          <PokemonDetailCardProfileTab
-            description={species.description}
-            abilities={abilities}
-            exp={data.exp}
-            happiness={species.happiness}
-            captureRate={species.captureRate}
-            growthRate={species.growthRate}
-            height={data.height}
-            weight={data.weight}
-          />
-        </Tab>
-        <Tab eventKey="stats" title="STATS">
-          <PokemonDetailCardStatsTab stats={data.stats} types={types} />
-        </Tab>
-        <Tab eventKey="evolutions" title="EVOLUTIONS">
-          <PokemonDetailCardEvolutionsTab chainID={species.evolutionChainId} type={data.types[0]} />
-        </Tab>
-      </Tabs>
-    </Card.Body>
-  </Card>
-);
+}) =>  {
+  const theme = useTheme()
+  const [ key, setKey ] = useState<string | null>('profile')
+
+  return (
+    <Card className={styles[theme === "light" ? "cardSplit" : "cardSplitDark"]}>
+      <Card.Body>
+        <Tabs 
+          activeKey={key} 
+          onSelect={(k) => setKey(k)} 
+          className={`${styles.cardTabs} ${styles[`cardTabsColor${types[0].name}`]} justify-content-center mb-4`}
+        >
+          <Tab eventKey="profile" title="PROFILE" tabClassName={key === 'profile' ? styles.cardTabsActive : ''}>
+            <PokemonDetailCardProfileTab
+              description={species.description}
+              abilities={abilities}
+              exp={data.exp}
+              happiness={species.happiness}
+              captureRate={species.captureRate}
+              growthRate={species.growthRate}
+              height={data.height}
+              weight={data.weight}
+            />
+          </Tab>
+          <Tab eventKey="stats" title="STATS" tabClassName={key === 'stats' ? styles.cardTabsActive : ''}>
+            <PokemonDetailCardStatsTab stats={data.stats} types={types} />
+          </Tab>
+          <Tab eventKey="evolutions" title="EVOLUTIONS" tabClassName={key === 'evolutions' ? styles.cardTabsActive : ''}>
+            <PokemonDetailCardEvolutionsTab chainID={species.evolutionChainId} type={data.types[0]} />
+          </Tab>
+        </Tabs>
+      </Card.Body>
+    </Card>
+  );
+}
 
 export default PokemonDetailCard;
