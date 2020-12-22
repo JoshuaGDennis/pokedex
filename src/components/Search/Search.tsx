3d77205ff8eb6d28953d1308545df1ff6364dba5
JoshuaGDennis <joshuagarydennis@gmail.com>
2020-12-22 21:18:06 +0000

    adding more search validation logic

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,22 @@
 import "./Search.scss";
+import { getAllPokemon } from "helpers/api";
+import Button from "react-bootstrap/Button";
 import Tooltip from "react-bootstrap/Tooltip";
 import Dropdown from "react-bootstrap/Dropdown";
 import { OverlayTrigger } from "react-bootstrap";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 
 const Search = () => {
   const [searchValue, setSearchValue] = useState("");
+
+  const [info, setInfo] = useState<string>("");
   const [errors, setErrors] = useState<string[]>([]);
+
+  const [pokemon, setPokemon] = useState<{ id: number; name: string }[]>([]);
+
+  useEffect(() => {
+    getAllPokemon().then(setPokemon);
+  }, []);
 
   const checkCondition = (condition: boolean, message: string) => {
     if (condition) {
@@ -33,6 +43,16 @@ const Search = () => {
     setSearchValue(value);
   };
 
+  const handleSubmit = () => {
+    if (!!searchValue && !errors.length) {
+      if (searchValue.length < 3) {
+        setInfo("Enter three or more characters!");
+      } else {
+        setInfo("");
+      }
+    }
+  };
+
   return (
     <Dropdown className="search-control">
       <div className={`search-control-inner ${!!errors.length ? "error" : ""}`}>
@@ -57,6 +77,9 @@ const Search = () => {
             <div className="search-control-error" />
           </OverlayTrigger>
         )}
+        <Button className="search-control-submit" onClick={handleSubmit}>
+          Search
+        </Button>
         <Dropdown.Toggle split className="search-control-options" />
       </div>
       <Dropdown.Menu>
